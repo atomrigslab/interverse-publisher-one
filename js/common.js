5b9c73608f9b8cc23bc42bef98623a68bf4d22b6
yong-atomrigs <yong@atomrigs.io>
2023-10-05 21:13:10 +0900

    add: nav design

--- js/common.js
+++ js/common.js
@@ -72,10 +72,10 @@ class NavigationBarElement extends AtomrigsElement {
     const homeUrl = `${urlPrefix}/${lang}`;
 
     this.innerHTML = `
-      <nav class="navbar navbar-expand-lg bg-body-tertiary" style="position:fixed; z-index:10; width:100%">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand" href="${homeUrl}">
-            Interverse
+            <img src="../assets/logo.png" />
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
