0b08812db08a39c0f29b3c8654f8c06bdb80d4a6
TS Kang <ts.kang@atomrigs.io>
2023-10-11 23:26:05 +0900

    모바일 레이아웃 수정정

--- js/components.js
+++ js/components.js
@@ -130,33 +130,48 @@ class NFTContainer extends HTMLElement {
         <div class="swiper-wrapper">
           <div class="swiper-slide mobile-slide">
 
-<div style="width: 100%; height: 100%; display: flex; flex-direction: column;
-  align-items: center; justify-content: space-between">
+            <div class="benefit-image">
+              <button
+                style="background: transparent; text-decoration: none;"
+                onClick="onBenefitBadgeClicked('${collection}', '${itemNo}')"
+              >
+                <img src="../assets/btn-benefit.png" />
+              </button>
+            </div>
 
-  <div style="width: 100%; height: calc(100% - 56px - 100px - 20px); margin-top: 56px; 
-    display: flex; align-items: center; justify-content: space-between; background: green">
-  
-    <div style="width: 50px; background: red">1</div>
-    <div style="height: 100%; width: calc(100% - 100px); 
-      display: flex; justify-content: center; align-items: center; background: orange;">
-
-      <img src="http://interverse.link/assets/ifland/img-ifland-cathelmet.png" 
-        style="width: 100%; height: 100%; object-fit: contain"
-        alt="" />
-
-    </div>
-    <div style="width: 50px; background: red">1</div>
-
-  </div>
-  <div style="width: 100%; height: 100px; background: blue">
-  
-  
-  </div>
-
-</div>
-
+            <atomrigs-nft-view
+              oneTimeImage="${oneTimeImage}"
+              collection="${collection}"
+              item-no="${itemNo}"
+            ></atomrigs-nft-view>
 
           </div>
+          
+          <div class="swiper-slide mobile-slide mobile-safari-full-height">
+            <div class="--a-full-view --a-flex-center">
+              <div class="--a-full-view">
+
+                <!-- 중요: 일부러 atomrigs-nft-view를 배경으로 깔아서 오버레이 된것 같은 효과를 줌 -->
+                <atomrigs-nft-view
+                  style="position: fixed; width: 100%;"
+                  oneTimeImage="${oneTimeImage}"
+                ></atomrigs-nft-view>
+
+                <div class="desc-container --a-flex-center">
+                  <div class="nft-desc-area">
+                    <div class="category"><span>${category}<span></div>
+                    <span class="subtitle">${subtitle}</span>
+                    <span class="title">${title}</span>
+                    <span class="content" style="margin-top: 1.25rem">${content}</span>
+                  </div>
+                  <button type="button" class="closeDescptionButton" onClick="closeDesc()">
+                    <img src="../assets/mobile/icon-close.svg" alt="" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     `;
@@ -178,49 +193,116 @@ class NFTView extends AtomrigsElement {
 
     const onViewIn3DClicked = collection ? `onClick="onViewIn3DClicked('${collection}', '${itemNo}')"` : '';
 
+    // this.innerHTML = `
+    //   <div class="mobile-page-container">
+    //     <div class="mobile-page-content">
+    //       <button type="button">
+    //         <img src="../assets/mobile/icon-To-Left.svg" />
+    //       </button>
+    //       <div style="flex-shrink: 1; width: 100%; display: flex; justify-content: center">
+    //         <img src="${oneTimeImage}" />
+    //       </div>
+    //       <button type="button">
+    //         <img src="../assets/mobile/icon-To-Right.svg" />
+    //       </button>
+    //     </div>
+    //     <div class="mobile-button-group">
+    //       <div class="mobile-button-group-row">
+    //         <button
+    //           type="button"
+    //           class="tool-button"
+    //           ${onViewIn3DClicked}
+    //         >
+    //           <img src="../assets/icon-3d_2.svg" style="display: inline-block" />${langObj['view']}
+    //         </button>
+    //         <button type="button" class="tool-button" onClick="onSampleClicked('${collection}', '${itemNo}')">
+    //           <img src="../assets/icon-if.svg" width="18" height="19" style="display: inline-block;" />${langObj['sample']}
+    //         </button>
+    //       </div>
+    //       <div class="mobile-button-group-row">
+    //         <button
+    //           type="button"
+    //           class="tool-button full-width"
+    //           style="background-image: linear-gradient(117deg, #FF007D 0%, #490FBB 100%)"
+    //           onClick="onPurchaseClicked('${collection}', '${itemNo}')"
+    //         >
+    //           <img src="../assets/icon-ifland.svg" />
+    //           <span>${langObj['purchase']}</span>
+    //         </button>
+    //       </div>
+    //     </div>
+    //     <div class="bottom-down-arrow-mobile">
+    //         <img class="icon-arrow" src="../assets/icon-arrow-mobile.svg" alt="" />
+    //       </div>
+    //   </div>
+    // `;
+
     this.innerHTML = `
-      <div class="mobile-page-container">
-        <div class="mobile-page-content">
-          <button type="button">
-            <img src="../assets/mobile/icon-To-Left.svg" />
-          </button>
-          <div style="flex-shrink: 1; width: 100%; display: flex; justify-content: center">
-            <img src="${oneTimeImage}" />
-          </div>
-          <button type="button">
-            <img src="../assets/mobile/icon-To-Right.svg" />
-          </button>
+      <div style="width: 100%; height: 100%; display: flex; flex-direction: column;
+        align-items: center; justify-content: space-between">
+
+        <div style="width: 100%; height: calc(100% - 56px - 200px - 20px); margin-top: 56px; 
+          display: flex; align-items: center; justify-content: space-between;">
+
+            <div style="width: 50px">
+              <button type="button">
+                <img src="../assets/mobile/icon-To-Left.svg" />
+              </button>
+            </div>
+            <div style="height: 100%; width: calc(100% - 100px); 
+              display: flex; justify-content: center; align-items: center">
+
+              <img src="${oneTimeImage}" 
+                style="width: 100%; height: 100%; object-fit: contain"
+                alt="" />
+
+            </div>
+            <div style="width: 50px">
+              <button type="button">
+                <img src="../assets/mobile/icon-To-Right.svg" />
+              </button>
+            </div>
+
         </div>
-        <div class="mobile-button-group">
-          <div class="mobile-button-group-row">
-            <button
-              type="button"
-              class="tool-button"
-              ${onViewIn3DClicked}
-            >
-              <img src="../assets/icon-3d_2.svg" style="display: inline-block" />${langObj['view']}
-            </button>
-            <button type="button" class="tool-button" onClick="onSampleClicked('${collection}', '${itemNo}')">
-              <img src="../assets/icon-if.svg" width="18" height="19" style="display: inline-block;" />${langObj['sample']}
-            </button>
+
+        <div style="width: 100%; height: 200px; 
+          display: flex; flex-direction: column; align-items: center;">
+
+          <div class="mobile-button-group">
+            <div class="mobile-button-group-row">
+              <button
+                type="button"
+                class="tool-button"
+                ${onViewIn3DClicked}
+              >
+                <img src="../assets/icon-3d_2.svg" style="display: inline-block" />${langObj['view']}
+              </button>
+              <button type="button" class="tool-button" onClick="onSampleClicked('${collection}', '${itemNo}')">
+                <img src="../assets/icon-if.svg" width="18" height="19" style="display: inline-block;" />${langObj['sample']}
+              </button>
+            </div>
+            <div class="mobile-button-group-row">
+              <button
+                type="button"
+                class="tool-button full-width"
+                style="background-image: linear-gradient(117deg, #FF007D 0%, #490FBB 100%)"
+                onClick="onPurchaseClicked('${collection}', '${itemNo}')"
+              >
+                <img src="../assets/icon-ifland.svg" />
+                <span>${langObj['purchase']}</span>
+              </button>
+            </div>
           </div>
-          <div class="mobile-button-group-row">
-            <button
-              type="button"
-              class="tool-button full-width"
-              style="background-image: linear-gradient(117deg, #FF007D 0%, #490FBB 100%)"
-              onClick="onPurchaseClicked('${collection}', '${itemNo}')"
-            >
-              <img src="../assets/icon-ifland.svg" />
-              <span>${langObj['purchase']}</span>
-            </button>
-          </div>
-        </div>
-        <div class="bottom-down-arrow-mobile">
+
+          <div class="bottom-down-arrow-mobile" style="margin: 0;">
             <img class="icon-arrow" src="../assets/icon-arrow-mobile.svg" alt="" />
           </div>
+    
+
+        </div>
+
       </div>
-    `;
+    `;    
   }
 
   connectedCallback() {
